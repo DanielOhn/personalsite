f3266b36a81526d44493f9c00efc2ae8caf472c8
DanielOhn <ohndaniel@gmail.com>
2022-03-16 12:41:42 -0700

    Init project

---
+++ src/App/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <div className="header">
+          <h1>Daniel <b className='lastname'>Ohn</b></h1>
+          
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <main className="content">
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
